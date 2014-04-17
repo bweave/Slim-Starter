46936b125e99427500d7855fcad541662b3c0e6a
Brian Weaver <brianweaver@Brians-MacBook-Pro-3.local>
2014-04-17 15:08:36 -0400

    Initial Commit

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'   => 'sqlite',
+    'database' => __DIR__ . '/../db.sqlite',
+    'prefix'   => '',
+    ]);
+
+$capsule->setAsGlobal();
+
+$capsule->bootEloquent();
